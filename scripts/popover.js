3affa7964b9c0ea2ebebfe25e56213134d9af0fe
Aleksandr Sesyolkin <sesyolkin@gmail.com>
2025-04-08 10:51:53 +0300

    added a popover

---
+++ scripts/popover.js
@@ -0,0 +1,32 @@
+import {computePosition, offset} from 'https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.6.13/+esm';
+
+const infoBadge = document.querySelector('.info-badge');
+const infoPopover = document.querySelector('.info-popover');
+
+function update() {
+  computePosition(infoBadge, infoPopover, {
+  placement: 'top-start',
+  middleware: [offset(10)]
+}).then(({x, y}) => {
+  Object.assign(infoPopover.style, {
+    left: `${x}px`,
+    top: `${y}px`,
+  });
+});
+}
+
+function showTooltip() {
+  infoPopover.style.display = 'block';
+  update();
+}
+
+function hideTooltip() {
+  infoPopover.style.display = '';
+}
+
+[
+  ['mouseenter', showTooltip],
+  ['mouseleave', hideTooltip]
+].forEach(([event, listener]) => {
+  infoBadge.addEventListener(event, listener);
+});
