d3881a56f7181cc0ea4529d4f83ea30c7dc4eeaf
Aleksandr Sesyolkin <sesyolkin@gmail.com>
2025-05-08 15:44:01 +0300

    adaptive layout for the results section

--- scripts/popover.js
+++ scripts/popover.js
@@ -1,4 +1,9 @@
-import {computePosition, offset} from 'https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.6.13/+esm';
+import {
+  computePosition, 
+  offset, 
+  shift,
+  flip
+} from 'https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.6.13/+esm';
 
 const infoBadge = document.querySelector('.info-badge');
 const infoPopover = document.querySelector('.info-popover');
@@ -6,13 +11,21 @@ const infoPopover = document.querySelector('.info-popover');
 function update() {
   computePosition(infoBadge, infoPopover, {
   placement: 'top-start',
-  middleware: [offset(10)]
-}).then(({x, y}) => {
-  Object.assign(infoPopover.style, {
-    left: `${x}px`,
-    top: `${y}px`,
+  middleware: [
+    offset(10), 
+    flip({
+      flipAlignment: false
+    }), 
+    shift({padding: {
+      right: 48,
+      left: 48
+    }})]
+  }).then(({x, y}) => {
+    Object.assign(infoPopover.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    });
   });
-});
 }
 
 function showTooltip() {
