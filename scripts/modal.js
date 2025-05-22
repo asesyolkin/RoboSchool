8be0aa9f27f1c682f7012aea58e693ef8e7a3d82
Aleksandr Sesyolkin <sesyolkin@gmail.com>
2025-05-22 10:52:19 +0300

    adaptive layout for a modal window

--- scripts/modal.js
+++ scripts/modal.js
@@ -5,15 +5,9 @@ const modal = document.querySelector('.modal'),
       modalCloseButton = modal.querySelector('.close-button'),
       trainerImg = modal.querySelector('.photo'),
       trainerName = modal.querySelector('.name'),
-      trainerProfession = modal.querySelector('.profession');
+      trainerProfession = modal.querySelector('.profession'),
+      isMobileModal = document.documentElement.clientWidth <= 660;
 let lastActiveTrainerId = 0;
-
-function modalClose(event) {
-  if (event.target == modal || event.target == modalCloseButton) {
-    modal.classList.add('hidden');
-    tabsButtons.forEach(elem => elem.classList.remove('empty'));
-  }
-};
 
 function modalOpen(event) {
   if (!event.target.closest('.card-of-trainer')) return; 
@@ -21,21 +15,12 @@ function modalOpen(event) {
   const trainerId = event.target.closest("[data-trainer-id]").dataset.trainerId;
 
   if (trainerId !== lastActiveTrainerId) {
-    tabHandler(0);
+    createTrainerContent(trainerId);
+    hideTabContent(activeTabId);
+    createTabsContent(trainerId);
+    showTabContent(0);
 
-    trainerImg.src = document.location.origin + dataTrainers[trainerId].img_src;
-    trainerImg.alt = document.location.origin + dataTrainers[trainerId].img_alt;
-    trainerName.innerHTML = dataTrainers[trainerId].full_name;
-    trainerProfession.innerHTML = dataTrainers[trainerId].profession;
-
-    tabsContent.forEach((elem, i) => {
-      const tabName = elem.dataset.tabContentName;
-
-      if (!dataTrainers[trainerId].tabs_content[tabName]) tabsButtons[i].classList.add('empty');
-      else elem.innerHTML = dataTrainers[trainerId].tabs_content[tabName];
-
-      new SimpleBar(elem, {autoHide: false }).getContentElement();
-    })
+    if (isMobileModal) createMobileTabButtonName(0);
 
     lastActiveTrainerId = trainerId;
   };
@@ -43,27 +28,145 @@ function modalOpen(event) {
   modal.classList.remove('hidden');
 }
 
+function createTrainerContent(trainerId) {
+  trainerImg.src = document.location.origin + dataTrainers[trainerId].img_src;
+  trainerImg.alt = dataTrainers[trainerId].img_alt;
+  trainerName.innerHTML = dataTrainers[trainerId].full_name;
+  trainerProfession.innerHTML = dataTrainers[trainerId].profession;
+}
+
+function modalClose(event) {
+  if (event.target == modal || event.target == modalCloseButton) {
+    modal.classList.add('hidden');
+
+    if (isMobileModal) closeTabList();
+  }
+};
+
+if (isMobileModal) {
+  modalCloseButton.textContent = '';
+}
+
 modal.onclick = modalClose;
 modalCloseButton.onclick = modalClose;
 document.querySelector('.swiper-wrapper').onclick = modalOpen;
 
 /* TABS */
-const tabsButtons = modal.querySelectorAll('[data-tab-button-id]'),
+const tabButtons = modal.querySelectorAll('[data-tab-button-id]'),
       tabsContent = modal.querySelectorAll('[data-tab-content-id]');
 let activeTabId = 0;
 
+// variables for the mobile version
+let tabList, tabIdBuffer, transitionInProcess, numberOfActiveTabs, mobileTabButton, heightOfmobileTabButton, arrow;
+
 function tabHandler(tabId) {
-  tabsButtons[activeTabId].classList.remove('active');
-  tabsButtons[tabId].classList.add('active');
+  hideTabContent(activeTabId);
+  showTabContent(tabId);
+  changeActiveTab(tabId);
+}
 
-  tabsContent[activeTabId].style.display = 'none';
-  tabsContent[tabId].style.display = 'block';
-
+function changeActiveTab(tabId) {
+  tabButtons[activeTabId].classList.remove('active');
+  tabButtons[tabId].classList.add('active');
   activeTabId = tabId;
 }
 
-tabsButtons.forEach(tab => {
-  tab.onclick = tabHandler.bind(null, tab.dataset.tabButtonId);
-});
+function createTabsContent (trainerId) {
+  activeTabId = 0;
+  tabButtons.forEach(elem => elem.classList.remove('active', 'empty'));
+  tabButtons[0].classList.add('active');
 
-tabHandler(activeTabId);
+  if (isMobileModal) numberOfActiveTabs = tabButtons.length - 1;
+  
+  tabsContent.forEach((elem, i) => {
+    const tabName = elem.dataset.tabContentName;
+
+    if (!dataTrainers[trainerId].tabs_content[tabName]) {
+      tabButtons[i].classList.add('empty');
+      if (isMobileModal) --numberOfActiveTabs;
+    }
+    else {
+      elem.innerHTML = dataTrainers[trainerId].tabs_content[tabName];
+    }
+
+    new SimpleBar(elem, {autoHide: false }).getContentElement();
+  })
+}
+
+function hideTabContent(tabId) {
+  tabsContent[tabId].style.display = 'none';
+}
+
+function showTabContent(tabId) {
+  tabsContent[tabId].style.display = 'block';
+}
+
+function mobileTabButtonHandler() {
+  if (transitionInProcess) return;
+
+  if (tabList.style.bottom === '') {
+    tabList.style.bottom = `-${heightOfmobileTabButton * numberOfActiveTabs}px`;
+  } else {
+    tabList.style.bottom = '';
+  }
+
+  rotateArrow();
+
+  transitionInProcess = true;
+}
+
+function rotateArrow() {
+  arrow.classList.toggle('show-tabs');
+}
+
+function tabListHandler() {
+  transitionInProcess = false;
+
+  if (!tabIdBuffer) return;
+
+  changeActiveTab(tabIdBuffer);
+
+  tabIdBuffer = null;
+}
+
+function mobileTabHandler(tabId) {
+  if (transitionInProcess) return;
+  
+  createMobileTabButtonName(tabId);
+  hideTabContent(activeTabId);
+  showTabContent(tabId);
+  closeTabList();
+  rotateArrow();
+  tabIdBuffer = tabId;
+  transitionInProcess = true;
+}
+
+function createMobileTabButtonName(tabId) {
+  mobileTabButton.children[0].textContent = tabButtons[tabId].textContent;
+}
+
+function closeTabList() {
+  tabList.style.bottom = '';
+}
+
+if (isMobileModal) {
+  tabIdBuffer = null;
+  transitionInProcess = false;
+  numberOfActiveTabs = 0;
+  tabList = modal.querySelector('.tabs'),
+  mobileTabButton = modal.querySelector('.mobile-tab-button'),
+  heightOfmobileTabButton = mobileTabButton.getClientRects()[0].height,
+  arrow = mobileTabButton.querySelector('.arrow');
+
+  tabList.ontransitionend = tabListHandler;
+
+  mobileTabButton.onclick = mobileTabButtonHandler;
+
+  tabButtons.forEach(tab => {
+    tab.onclick = mobileTabHandler.bind(null, tab.dataset.tabButtonId);
+  });
+} else {
+  tabButtons.forEach(tab => {
+    tab.onclick = tabHandler.bind(null, tab.dataset.tabButtonId);
+  });
+}
